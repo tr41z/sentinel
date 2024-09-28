93097d1dbc4843989793b0ba1be038015a418a47
traizooo <michal.owsiejew23@gmail.com>
2024-09-28 13:13:29 +0200

    Commit

--- src-tauri/src/database/db.rs
+++ src-tauri/src/database/db.rs
@@ -8,12 +8,12 @@ use std::{env, fs};
 async fn connect() -> Result<Pool<MySql>, Error> {
     dotenv().ok();
 
-    let username = env::var("DB_USERNAME").expect("'DB_USERNAME' must be set!");
-    let password = env::var("DB_PASSWORD").expect("'DB_PASSWORD' must be set!");
-    let db_name = env::var("DB_NAME").expect("'DB_NAME' must be set!");
-    let db_url = env::var("DB_URL").expect("'DB_URL' must be set!");
+    let username: String = env::var("DB_USERNAME").expect("'DB_USERNAME' must be set!");
+    let password: String = env::var("DB_PASSWORD").expect("'DB_PASSWORD' must be set!");
+    let db_name: String = env::var("DB_NAME").expect("'DB_NAME' must be set!");
+    let db_url: String = env::var("DB_URL").expect("'DB_URL' must be set!");
 
-    let connection_string = format!("mysql://{}:{}@{}/{}", username, password, db_url, db_name);
+    let connection_string: String = format!("mysql://{}:{}@{}/{}", username, password, db_url, db_name);
     return MySqlPool::connect(&connection_string).await;
 }
 
@@ -28,10 +28,10 @@ async fn initialise_schema(pool: &MySqlPool) -> Result<(), Error> {
 }
 
 #[allow(unused)]
-async fn save_flow(pool: &MySqlPool) -> Result<(), Error> {Ok(())}
+pub async fn save_flow(pool: &MySqlPool) -> Result<(), Error> {Ok(())}
 
 pub async fn do_test_connection() {
-    let res = task::block_on(connect());
+    let res: Result<Pool<MySql>, Error> = task::block_on(connect());
 
     match res {
         Err(err) => {
