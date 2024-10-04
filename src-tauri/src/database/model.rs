6f0b0a56752b1c63170336d4275778f68e7fc96f
traizooo <michal.owsiejew23@gmail.com>
2024-10-04 18:13:34 +0100

    Refactor DataModel struct to improve readability and maintainability

--- src-tauri/src/database/model.rs
+++ src-tauri/src/database/model.rs
@@ -23,8 +23,10 @@ pub struct DataModel {
 
 impl DataModel {
     pub fn new(
-        src_ip: Ipv4Addr, dst_ip: Ipv4Addr,
-        src_port: u16, dst_port: u16,
+        src_ip: Ipv4Addr, 
+        dst_ip: Ipv4Addr,
+        src_port: u16, 
+        dst_port: u16,
         protocol: u8,
         total_bytes: u64,
         total_packet_count: u32,
