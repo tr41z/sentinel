8f9868a4eae1f8427afe3cb9e795e8f664c209bc
traizooo <michal.owsiejew23@gmail.com>
2024-12-15 15:21:12 +0100

    implementing multithreading for capturing packets from multiple interfaces at the same time; new executables; adjusting comments

--- sniffer-mod/src/main.c
+++ sniffer-mod/src/main.c
@@ -4,14 +4,8 @@
 
 int main() {
   char *home_dir = get_home_dir();
-  devPtr dev = find_devices();
-
   connect_db(home_dir);
-  if (!dev) {
-    printf("No devices found or an error occured!\n");
-    return 1;
-  }
 
-  start_sniffer(dev);
+  start_multithreaded_sniffing();
   return 0;
 }
