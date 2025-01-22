e9bec41b7c2bace57fcd62fc50f23a14ffc65e81
tr41z <michal.owsiejew23@gmail.com>
2025-01-22 20:18:12 +0100

    refactor: update go.mod and improve main.go for async execution

--- src-back/go.mod
+++ src-back/go.mod
@@ -2,4 +2,4 @@ module backend
 
 go 1.23.5
 
-require github.com/mattn/go-sqlite3 v1.14.24 // indirect
+require github.com/mattn/go-sqlite3 v1.14.24
--- src-back/main.go
+++ src-back/main.go
@@ -12,7 +12,8 @@ func main() {
 	db.InitDB()
 	http.HandleFunc("/api/v1/flows", db.FetchFlows)
 
+	go executable.Invoke(); // async invoke of sniffer process
+
 	fmt.Println("Server is running at http://localhost:8080")
  	log.Fatal(http.ListenAndServe(":8080", nil)) // Start the server on port 8080
-	executable.Invoke();
 }
