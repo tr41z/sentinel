ec94aa91ac4f8e98c41e42b5f61cedb4999b21f7
tr41z <michal.owsiejew23@gmail.com>
2025-02-26 14:04:20 +0100

    adding test compilation in `CMakeLists`; adding preprocessing functions; adding more tests to each

--- sniffer-mod/src/include/prep.h
+++ sniffer-mod/src/include/prep.h
@@ -2,15 +2,15 @@
 #define PREP_H
 
 #include <unordered_set>
+#include <cstdint>
 
 typedef std::unordered_set<uint16_t> portSet;
 
+double round_to(double value, int decimal_places);
 int count_ports_in_range(portSet ports, int low, int high);
-int count_unique_ports(portSet ports);
-float calculate_entropy(portSet ports);
-float calculate_pps(int total_packet_count, int duration); // Packets per sec
-float calculate_bps(int total_bytes, int duration); // Bytes per sec
-float calculate_avg_bpp(int total_bytes, int total_packet_count); // Bytes per packet
+double calculate_pps(int total_packet_count, int duration); // Packets per sec
+double calculate_avg_bpp(int total_bytes, int total_packet_count); // Bytes per packet
+double calculate_rate(int total_bytes, int duration);
 int is_brute_target(portSet ports);
 int is_dos_target(portSet ports);
 
--- sniffer-mod/src/prep.cpp
+++ sniffer-mod/src/prep.cpp
@@ -1,16 +1,28 @@
 #include "include/prep.h"
+#include <cmath>
+
+double round_to(double value, int decimal_places) {
+    double factor = std::pow(10.0, decimal_places);
+    return std::round(value * factor) / factor;
+} 
 
 int count_ports_in_range(portSet ports, int low, int high) {
-    int count = 0;
+    if (ports.empty()) {
+        return 0;
+    }
 
-    while (high > low) {
-        for (int i = 0; i <= high; i++) {
-            if (ports.find(i) != ports.end()) {
-                count++;
-                low++;
-            }
-            low++;
+    int count = 0;
+    for (int i = low; i <= high; i++) {
+        if (ports.find(i) != ports.end()) {
+            count++;
         }
     }
     return count;
 }
+
+double calculate_pps(int total_packet_count, int duration) {
+    return duration > 0 ?
+        round_to(static_cast<double>(total_packet_count) / duration, 5) 
+        : total_packet_count; 
+}
+
