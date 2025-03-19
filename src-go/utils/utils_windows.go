0f310e7ff7876ac41187608288eebff8c8b634fd
tr41z <michal.owsiejew23@gmail.com>
2025-03-19 08:41:57 +0100

    Update executable paths and increase prediction interval for improved functionality

--- src-go/utils/utils_windows.go
+++ src-go/utils/utils_windows.go
@@ -2,5 +2,6 @@
 
 package utils
 
-const EXECUTABLE_PATH = "..\\sniffer-mod\\build\\sniffer.exe"
+const SNIFFER_EXECUTABLE_PATH = "..\\sniffer-mod\\build\\sniffer.exe"
+const AI_EXECUTABLE_PATH = "..\\ai-mod\bin\\main.exe"
 const DB_PATH = "\\.sentinel\\sentinel.db"
